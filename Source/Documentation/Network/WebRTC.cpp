d0ad81ce81f5921fa9fbc403d90cf5450aa05811
Your Name <you@example.com>
2025-11-12 23:43:53 +0800

    webrtc

--- Source/Documentation/Network/WebRTC.cpp
+++ Source/Documentation/Network/WebRTC.cpp
@@ -1,5 +1,16 @@
 ﻿
 /*
+	1. 链接
+		https://chromium.googlesource.com/external/webrtc/
+		Official web site: http://www.webrtc.org
+		Master source code repo: https://webrtc.googlesource.com/src
+		Samples and reference apps: https://github.com/webrtc
+		Mailing list: http://groups.google.com/group/discuss-webrtc
+		Continuous build: https://ci.chromium.org/p/webrtc/g/ci/console
+		Coding style guide: https://chromium.googlesource.com/external/webrtc/+/HEAD/g3doc/style-guide.md
+		Code of conduct: https://chromium.googlesource.com/external/webrtc/+/HEAD/CODE_OF_CONDUCT.md
+
+	2.
 	webrtc::PeerConnectionInterface
 	webrtc::IceCandidateInterface	
 */
